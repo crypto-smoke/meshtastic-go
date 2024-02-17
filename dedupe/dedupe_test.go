0cb6a525963566f80f9e91dc42356950d195a426
Dustin Sallings <dustin@spy.net>
2024-02-17 09:10:14 -1000

    A bit structurally simpler deduplication
    No strings and more tests

--- dedupe/dedupe_test.go
+++ dedupe/dedupe_test.go
@@ -1,16 +1,15 @@
-package dedupe_test
+package dedup
 
 import (
-	"crypto/md5"
-	"github.com/crypto-smoke/meshtastic-go/dedupe"
 	"testing"
+	"testing/quick"
 	"time"
 )
 
+const expiresAfter = time.Millisecond
+
 func TestPacketDeduplicatorSeen(t *testing.T) {
-	hasher := md5.New()
-	expiresAfter := 100 * time.Millisecond
-	dedup := dedupe.NewDeduplicator(hasher, expiresAfter)
+	dedup := NewDeduplicator(expiresAfter)
 
 	sender := uint32(1)
 	packetID := uint32(1)
@@ -26,7 +25,7 @@ func TestPacketDeduplicatorSeen(t *testing.T) {
 	}
 
 	// Wait for expiration
-	time.Sleep(expiresAfter + 100*time.Millisecond)
+	time.Sleep(expiresAfter + time.Millisecond)
 
 	// Test Seen with same packetID after expiration
 	if dedup.Seen(sender, packetID) {
@@ -34,10 +33,44 @@ func TestPacketDeduplicatorSeen(t *testing.T) {
 	}
 }
 
+func TestDuplicatorProp(t *testing.T) {
+	if err := quick.Check(func(s, p uint32) bool {
+		dedup := NewDeduplicator(expiresAfter)
+		// Test Seen with new packetID
+		if dedup.Seen(s, p) {
+			t.Error("Expected the packet to not have been seen")
+			return false
+		}
+
+		// Test Seen with same packetID again
+		if !dedup.Seen(s, p) {
+			t.Error("Expected the packet to have been seen")
+			return false
+		}
+		return true
+	}, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func FuzzDup(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s uint32, p uint32) {
+		dedup := NewDeduplicator(10*time.Second)
+		// Test Seen with new packetID
+		if dedup.Seen(s, p) {
+			t.Errorf("Expected the packet %v/%v to not have been seen the first time", s, p)
+		}
+
+		// Test Seen with same packetID again
+		if !dedup.Seen(s, p) {
+			t.Errorf("Expected the packet %v/%v to have been seen", s, p)
+		}
+
+	})
+}
+
 func TestPacketDeduplicatorSeenData(t *testing.T) {
-	hasher := md5.New()
-	expiresAfter := 100 * time.Millisecond
-	dedup := dedupe.NewDeduplicator(hasher, expiresAfter)
+	dedup := NewDeduplicator(expiresAfter)
 
 	data := []byte("test data")
 
@@ -52,7 +85,7 @@ func TestPacketDeduplicatorSeenData(t *testing.T) {
 	}
 
 	// Wait for expiration
-	time.Sleep(expiresAfter + 100*time.Millisecond)
+	time.Sleep(expiresAfter + time.Millisecond)
 
 	// Test SeenData with same data after expiration
 	if dedup.SeenData(data) {
